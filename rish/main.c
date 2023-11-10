baf394d3009bf86f55b5444f7494285368ff58f4
rgege <rokas@rgl.lt>
2023-11-10 19:18:11 +0200

    work on shell

--- rish/main.c
+++ rish/main.c
@@ -2,45 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
+#include <sys/wait.h>
 
 #define BUFSIZE 32
 
 char *argvector[BUFSIZE];
+char *searchpath[BUFSIZE];
 
 void error(void);
 
-void
-cd(char *s)
+int
+cd(void)
 {
-	return 
+	if (chdir(argvector[1]) == -1) {
+		error();
+		return -1;
+	} else
+		return 0;
 }
 
 int
-builtin(void)
+path(void)
 {
-	if (strcmp(argvector[0], "cd") == 0)
-		return 1;
-	else if (strcmp(argvector[0], "path") == 0)
-		return 2;
-	else if (strcmp(argvector[0], "ls") == 0)
-		return 3;
-	else
-		return -1;
+	char **pav = argvector;
+	char **psp = searchpath;
+
+	while ((*psp++ = *++pav))
+		;
+	return 0;
 }
 
-void
+int
+builtin(int c)
+{
+	if (strcmp(argvector[0], "cd") == 0) {
+		if (c != 2) {
+			error();
+			return -1;
+		} else			
+			return 1;
+	}
+	else if (strcmp(argvector[0], "path") == 0)
+		return 2;
+	else if (strcmp(argvector[0], "exit") == 0)
+		return 3;
+	else
+		return 4;
+}
+
+int
 lexer(char *s)
 {
 	char **ap;
 	char **p = argvector;
+	int count = 0;
 
 
-	for (ap = argvector; ap < &argvector[9] &&
+	for (ap = argvector; ap < &argvector[BUFSIZE] &&
 	    (*ap = strsep(&s, " ")) != NULL;) {
-		if (**ap != '\0')
-			ap++;
+		if (**ap != '\0') {
+			++count;
+			++ap;
+		}
 	}
 	*ap = NULL;
+	return count;
 }
 
 void
@@ -53,7 +80,7 @@ error(void)
 char *
 getinput(void)
 {
-	char buf[BUFSIZE];
+	char buf[PATH_MAX];
 	char *p = buf;
 
 	printf("rish$> ");
@@ -64,6 +91,25 @@ getinput(void)
 		buf[strcspn(buf, "\n")] = '\0';
 	return p;
 }
+
+void
+execute(void)
+{
+	char s[] = "/bin/";
+	char path[PATH_MAX];
+	strcpy(path, s);
+	printf("%s\n", path);
+
+	char *a[] = {argvector[0], argvector[1], NULL};
+	int rc;
+
+	if ((rc = fork()) == -1) 
+		error();
+	else if (rc == 0)
+		execv("bin/ls", a);
+	else
+		wait(NULL);
+}
 	
 int
 main(int argc, char *argv[])
@@ -72,23 +118,25 @@ main(int argc, char *argv[])
 
 	char *s;
 	int c;
-	char **a;
+	int cnt;
 	
 	while (strcmp(s, e)) {
 		s = getinput();
-		lexer(s);
-		c = builtin();
+		cnt = lexer(s);
+		c = builtin(cnt);
 
 		switch(c) {
 			case 1:
-				cd(argvector[1]);
+				cd();
 				break;
 			case 2:
+				path();
 				break;
 			case 3:
-				system("ls");
+				exit(0);
 				break;
 			default:
+				execute();
 				break;
 		}
 	}
